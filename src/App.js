f592a7f0d9df0f85ce51d1fea65f6bddb181603c
Sean Daly <daly.se91@gmail.com>
2021-03-04 10:48:56 -0600

    Tested ability to pull from local graphql server - Sucess

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import React from 'react'
+import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client'
+import {gql,  useQuery} from '@apollo/client'
 import './App.css';
 
 function App() {
+  
+  const User = gql`
+      {
+        users{
+          _id
+          name
+        }
+      }`
+    
+  
+  const GetUsers = () =>{
+    const {loading, error, data}= useQuery(User)
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+  return data.users.map((ele) => (
+    <div >
+      <h2>This is gql query</h2>
+      <p>
+        {ele._id}<br/>
+        {ele.name}
+      </p>
+    </div>
+  ));
+  }
+
   return (
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       {GetUsers()}
       </header>
     </div>
+    
   );
 }
 
